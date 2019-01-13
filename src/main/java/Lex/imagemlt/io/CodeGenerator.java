dfd15dbb4243f1abd1b57b85c3ee5a271789c610
imagemlt <malingtao1019@163.com>
2019-01-13 15:39:24 +0800

    修复了一处|号的解析错误

--- src/main/java/Lex/imagemlt/io/CodeGenerator.java
+++ src/main/java/Lex/imagemlt/io/CodeGenerator.java
@@ -9,9 +9,7 @@ public class CodeGenerator {
     private static String head="import javafx.util.Pair;\n" +
             "\n" +
             "import java.io.BufferedReader;\n" +
-            "import java.io.InputStream;\n" +
-            "import java.io.InputStreamReader;\n" +
-            "import java.io.Reader;\n" +
+            "import java.io.*;\n" +
             "import java.util.*;\n" +
             "\n" +
             "\n" +
